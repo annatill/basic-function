e131aff3e075bcd596e8b1009aff8d1fd6ad149a
Siracenco Serghei <siracencoserghei@gmail.com>
2023-05-04 21:09:30 +0200

    sergio

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,7 @@ import {
 import forbes from 'data/forbes.json';
 import article from 'data/article.json';
 import data from 'data/data.json';
+import transactions from '../../data/transactions.json';
 
 
 export const App = () => {
@@ -40,7 +41,8 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
+        
       </Container>
     </Section>
   );
--- src/servises/data.js
+++ src/servises/data.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { format, formatDistanceToNow } from 'date-fns';
 
 export function createData(data) {
   return formatDistanceToNow(new Date(data), { addSuffix: true });
+}
+
+export const datapmam = (date) => {
+  return format(new Date(date), 'Pp');
 }
